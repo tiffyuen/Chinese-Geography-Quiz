a8b9aef5b7f8937ef115216e8cc2c0931f4c2444
Tiffany Yuen <tiffyuen@users.noreply.github.com>
2017-08-22 16:07:01 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,121 +1,70 @@
+// Modified from Amazon Alexa Template
+// Author: Tiffany Yuen
+
 'use strict';
 const Alexa = require('alexa-sdk');
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this:  var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = "amzn1.ask.skill.5ebd34d6-9d09-46ee-9c94-5e53ac2c175b";
 
-//This function returns a descriptive sentence about your data.  Before a user starts a quiz, they can ask about a specific data element,
-//like "Ohio."  The skill will speak the sentence from this function, pulling the data values from the appropriate record in your data.
+// Before a user starts a quiz, they can ask about a specific data element, like "Shanghai."
+// The skill will speak the sentence from this function, pulling the data values from the appropriate record in data.
 function getSpeechDescription(item)
 {
     var sentence = "The capital of " + item.StateName + " is " + item.Capital + ", and the abbreviation for " + item.StateName + " is <break strength='strong'/><say-as interpret-as='spell-out'>" + item.Abbreviation + "</say-as>.  I've added " + item.StateName + " to your Alexa app.  Which other state or capital would you like to know about?";
     return sentence;
 }
 
-//We have provided two ways to create your quiz questions.  The default way is to phrase all of your questions like: "What is X of Y?"
-//If this approach doesn't work for your data, take a look at the commented code in this function.  You can write a different question
-//structure for each property of your data.
+// Defines questions' structure
 function getQuestion(counter, property, item)
 {
     return "Here is your " + counter + "th question.  What is the " + formatCasing(property) + " of "  + item.StateName + "?";
-
-    /*
-    switch(property)
-    {
-        case "City":
-            return "Here is your " + counter + "th question.  In what city do the " + item.League + "'s "  + item.Mascot + " play?";
-        break;
-        case "Sport":
-            return "Here is your " + counter + "th question.  What sport do the " + item.City + " " + item.Mascot + " play?";
-        break;
-        case "HeadCoach":
-            return "Here is your " + counter + "th question.  Who is the head coach of the " + item.City + " " + item.Mascot + "?";
-        break;
-        default:
-            return "Here is your " + counter + "th question.  What is the " + formatCasing(property) + " of the "  + item.Mascot + "?";
-        break;
-    }
-    */
 }
 
-//This is the function that returns an answer to your user during the quiz.  Much like the "getQuestion" function above, you can use a
-//switch() statement to create different responses for each property in your data.  For example, when this quiz has an answer that includes
-//a state abbreviation, we add some SSML to make sure that Alexa spells that abbreviation out (instead of trying to pronounce it.)
+// Returns an answer to user during the quiz
 function getAnswer(property, item)
 {
-    switch(property)
-    {
-        case "Abbreviation":
-            return "The " + formatCasing(property) + " of " + item.StateName + " is <say-as interpret-as='spell-out'>" + item[property] + "</say-as>. "
-        break;
-        default:
-            return "The " + formatCasing(property) + " of " + item.StateName + " is " + item[property] + ". "
-        break;
-    }
+    return "The " + formatCasing(property) + " of " + item.StateName + " is " + item[property] + ". "
+
 }
 
-//This is a list of positive speechcons that this skill will use when a user gets a correct answer.  For a full list of supported
-//speechcons, go here: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
+// This is a list of positive speechcons that this skill will use when a user gets a correct answer
+// Adapted from https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
 var speechConsCorrect = ["Booya", "All righty", "Bam", "Bazinga", "Bingo", "Boom", "Bravo", "Cha Ching", "Cheers", "Dynomite",
 "Hip hip hooray", "Hurrah", "Hurray", "Huzzah", "Oh dear.  Just kidding.  Hurray", "Kaboom", "Kaching", "Oh snap", "Phew",
 "Righto", "Way to go", "Well done", "Whee", "Woo hoo", "Yay", "Wowza", "Yowsa"];
 
-//This is a list of negative speechcons that this skill will use when a user gets an incorrect answer.  For a full list of supported
-//speechcons, go here: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
+// This is a list of negative speechcons that this skill will use when a user gets an incorrect answer
+// Adapted from https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speechcon-reference
 var speechConsWrong = ["Argh", "Aw man", "Blarg", "Blast", "Boo", "Bummer", "Darn", "D'oh", "Dun dun dun", "Eek", "Honk", "Le sigh",
 "Mamma mia", "Oh boy", "Oh dear", "Oof", "Ouch", "Ruh roh", "Shucks", "Uh oh", "Wah wah", "Whoops a daisy", "Yikes"];
 
-//This is the welcome message for when a user starts the skill without a specific intent.
+// This is the welcome message for when a user starts the skill without a specific intent.
 var WELCOME_MESSAGE = "Welcome to the China Quiz Game!  You can ask me about any of the four municipalities, five autonomous regions, two special administrative regions, provinces and their capitals, or you can ask me to start a quiz.  What would you like to do?";
 
-//This is the message a user will hear when they start a quiz.
+// This is the message a user will hear when they start a quiz.
 var START_QUIZ_MESSAGE = "OK.  I will ask you 10 questions about China.";
 
-//This is the message a user will hear when they try to cancel or stop the skill, or when they finish a quiz.
+// This is the message a user will hear when they try to cancel or stop the skill, or when they finish a quiz.
 var EXIT_SKILL_MESSAGE = "Thank you for playing the China Geography Quiz Game!  Let's play again soon!";
 
-//This is the message a user will hear after they ask (and hear) about a specific data element.
+// This is the message a user will hear after they ask (and hear) about a specific data element.
 var REPROMPT_SPEECH = "Which other province or capital would you like to know about?";
 
-//This is the message a user will hear when they ask Alexa for help in your skill.
-var HELP_MESSAGE = "I know lots of things about the China.  You can ask me about a province or a capital, and I'll tell you what I know.  You can also test your knowledge by asking me to start a quiz.  What would you like to do?";
+// This is the message a user will hear when they ask Alexa for help in your skill.
+var HELP_MESSAGE = "I know lots of things about the China. You can ask me about a province or a capital, and I'll tell you what I know.  You can also test your knowledge by asking me to start a quiz.  What would you like to do?";
 
 
-//This is the response a user will receive when they ask about something we weren't expecting.  For example, say "pizza" to your
-//skill when it starts.  This is the response you will receive.
+// This is the response a user will receive when they ask about something we weren't expecting.
+// For example, say "pizza" to the skill when it starts.  This is the response you will receive.
 function getBadAnswer(item) { return "I'm sorry. " + item + " is not something I know very much about in this skill. " + HELP_MESSAGE; }
 
 //This is the message a user will receive after each question of a quiz.  It reminds them of their current score.
 function getCurrentScore(score, counter) { return "Your current score is " + score + " out of " + counter + ". "; }
 
-//This is the message a user will receive after they complete a quiz.  It tells them their final score.
+// This is the message a user will receive after they complete a quiz.  It tells them their final score.
 function getFinalScore(score, counter) { return "Your final score is " + score + " out of " + counter + ". "; }
 
-//These next four values are for the Alexa cards that are created when a user asks about one of the data elements.
-//This only happens outside of a quiz.
-
-//If you don't want to use cards in your skill, set the USE_CARDS_FLAG to false.  If you set it to true, you will need an image for each
-//item in your data.
-var USE_CARDS_FLAG = true;
-
-//This is what your card title will be.  For our example, we use the name of the state the user requested.
-function getCardTitle(item) { return item.StateName;}
-
-//This is the small version of the card image.  We use our data as the naming convention for our images so that we can dynamically
-//generate the URL to the image.  The small image should be 720x400 in dimension.
-function getSmallImage(item) { return "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/720x400/" + item.Abbreviation + "._TTH_.png"; }
-
-//This is the large version of the card image.  It should be 1200x800 pixels in dimension.
-function getLargeImage(item) { return "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/alexa-skills-kit/tutorials/quiz-game/state_flag/1200x800/" + item.Abbreviation + "._TTH_.png"; }
-
-//=========================================================================================================================================
-//TODO: Replace this data with your own.
-//=========================================================================================================================================
+// Quiz data bank
 var data = [
                 {StateName: "Beijing Municipality",        Abbreviation: "Jing", Capital: "Beijing"},
                 {StateName: "Tianjin Municipality",         Abbreviation: "Jīn", Capital: "Tianjin"},
@@ -152,10 +101,6 @@ var data = [
                 {StateName: "Hong Kong Special Administrative Region", Abbreviation: "Gang", Capital: "Hong Kong" },
                 {StateName: "Macau Special Administrative Region",   Abbreviation: "Ao", Capital: "Macau"}
             ];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
 
 var counter = 0;
 
@@ -196,15 +141,7 @@ var startHandlers = Alexa.CreateStateHandler(states.START,{
         if (item && item[Object.getOwnPropertyNames(data[0])[0]] != undefined)
         {
           console.log("\nMEMO's TEST\n");
-            if (USE_CARDS_FLAG)
-            {
-                var imageObj = {smallImageUrl: getSmallImage(item), largeImageUrl: getLargeImage(item)};
-                this.emit(":askWithCard", getSpeechDescription(item), REPROMPT_SPEECH, getCardTitle(item), getTextDescription(item), imageObj);
-            }
-            else
-            {
-                this.emit(":ask", getSpeechDescription(item), REPROMPT_SPEECH);
-            }
+          this.emit(":ask", getSpeechDescription(item), REPROMPT_SPEECH);
         }
         else
         {
